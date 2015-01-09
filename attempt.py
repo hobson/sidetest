ec363d89a7de818f2e06c46d84f9a958ad155324
Hobson Lane <hobsonlane@gmail.com>
2015-01-09 08:24:51 -0800

    load and analyze functions work and pass asserts

--- attempt.py
+++ attempt.py
@@ -1,3 +1,7 @@
+import json
+from collections import Counter
+import pandas as pd
+
 def query_walgreens():
     """Return at most 5 product records for stores in or near the indicated zipcode
 
@@ -17,6 +21,47 @@ def query_walgreens():
 def mirror_walgreens():
    """Generate all retail products (and their store zip codes) for Walgreens
 
-   A correct and complete strategy would be to query a zipcode not in those already found for each individual product until all zipcodes have been queried at least once for each product. This seem like an O(N*M) solution, where N is the number of zipcodes and M the number of product types.
+   A correct and complete strategy would be to query a zipcode not in those already found for each individual product until all zipcodes have been queried at least once for each product. This seem like an O(N*M*P) solution, where N = # zipcodes, M = # product types, P = # pages.
+   But worst case optimal solution would be O(N*M) if no duplicate product-store pairs were returned for the same zipcode query.
+   To reduce duplicate queries need to maintain list of all possible N*M  product-store combinations
 
    """
+
+
+def index_products(path='gistfile1.json'):
+    """Index (create hash table) of a table of records from a json file"""
+    js = json.load(open(path, 'r'))
+    labels = js[0].keys()
+    keys = ('product_id', 'store_id')
+    store_products = {}
+    for rec in js:
+        i = '|'.join(rec[k] for k in keys)
+        # need to make sure all the columns are processed in the same order to reuse the canonical labels list
+        store_products[i] = [rec[k] for k in labels]
+    assert(len(js)==len(store_products))
+    return store_products
+
+
+
+def load_dataframe(path='gistfile1.json'):
+    """Create a pandas dataframe from a list of objects in a json file"""
+    js = json.load(open(path, 'r'))
+    df = pd.DataFrame.from_records(js)
+    print df.describe()
+    assert(len(df)==len(js))
+    return df
+    keys = ('product_id', 'store_id')
+    store_products = {}
+    for rec in js:
+        i = '|'.join(rec[k] for k in keys)
+        store_products[i] = rec
+
+
+def analyze(df=None):
+    df = df or load_dataframe()
+    print df.describe()
+    for col in df.columns:
+        c = Counter(df[col])
+        print '========== {N} ==========\n{col} item: count...'.format(N=len(c), col=col)
+        print dict((k,v) for i,(k,v) in enumerate(c.iteritems()) if i < 20)
+
